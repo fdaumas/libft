cad8716b8983ca5e619220a351e5a243dd76b672
fdaumas <fdaumas@student.42lyon.fr>
2021-11-15 16:55:54 +0100

    strtrim opti + secure

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: fdaumas <fdaumas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 17:49:36 by fdaumas           #+#    #+#             */
-/*   Updated: 2021/11/15 13:37:29 by fdaumas          ###   ########.fr       */
+/*   Updated: 2021/11/15 16:54:35 by fdaumas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,29 +51,14 @@ int	nb_char(char const *s1, char const *set)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*trim;
 	int		index;
 	int		nb_c;
-	int		index_trim;
 
-	index_trim = 0;
+	if (!s1)
+		return (0);
 	index = 0;
 	nb_c = nb_char(s1, set);
-	if (nb_c <= 0)
-	{
-		trim = malloc(sizeof(char));
-		if (!trim)
-			return (0);
-		trim[0] = '\0';
-		return (trim);
-	}
-	trim = malloc(sizeof(char) * nb_c + 1);
-	if (!trim)
-		return (0);
 	while (s1[index] && is_set(s1[index], set) == 1)
 		index++;
-	while (index_trim <= nb_c)
-		trim[index_trim++] = s1[index++];
-	trim[nb_c] = '\0';
-	return (trim);
+	return (ft_substr(s1, index, nb_c));
 }
