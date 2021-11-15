e0fc72dd5b0cf0958f1d08d17ccc98a1235df176
fdaumas <fdaumas@student.42lyon.fr>
2021-11-15 18:08:07 +0100

    split finito substr corrige itoa commence

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,40 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fdaumas <fdaumas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/10 11:23:15 by fdaumas           #+#    #+#             */
-/*   Updated: 2021/11/15 17:26:18 by fdaumas          ###   ########.fr       */
+/*   Created: 2021/11/15 17:16:05 by fdaumas           #+#    #+#             */
+/*   Updated: 2021/11/15 18:06:40 by fdaumas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_itoa(int n)
 {
-	size_t	index;
-	char	*ns;
+	char		tmp[12];
+	char		*new;
+	long long	nb;
+	int			unit;
+	int			index;
 
-	if (!s)
-		return (0);
 	index = 0;
-	while (s[start + index])
-		index++;
-	if (index < len)
-		len = index;
-	ns = malloc(sizeof(char) * len + 1);
-	if (!ns)
-		return (NULL);
-	index = 0;
-	if (start > ft_strlen(s))
+	if (n < 0)
 	{
-		ns[0] = '\0';
-		return (ns);
+		nb = n * -1;
+		tmp[index++] = '-';
 	}
-	while (index < len && s[start])
-		ns[index++] = s[start++];
-	ns[index] = '\0';
-	return (ns);
+	while (n / 10 > 0)
+	{
+		unit = n % 10;
+		n = n / 10;
+		tmp[index] = unit;
+		index++;
+	}
+	tmp[index] = '\0';
+	new = malloc (sizeof(char) * ft_strlen(tmp) + 1);
+	if (!new)
+		return (NULL);
+}
+
+int main()
+{
+	char *test;
+
+	test = ft_itoa(-2147483648);
+	printf("%s\n", test);
+	free(test);
 }
