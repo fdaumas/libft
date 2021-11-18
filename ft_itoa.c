c8f3d080400814a8e03fd99dc1533bb8c86213cf
fdaumas <florian.daumas.git@gmail.com>
2021-11-18 22:44:35 +0100

    itoa finish

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,55 +6,68 @@
 /*   By: fdaumas <fdaumas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 17:16:05 by fdaumas           #+#    #+#             */
-/*   Updated: 2021/11/16 21:44:38 by fdaumas          ###   ########.fr       */
+/*   Updated: 2021/11/18 22:43:57 by fdaumas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*reverse(char *tmp)
+size_t	nbr(char *new, long long nb)
 {
-	size_t		index;
-	char	*new;
+	int		unit;
+	size_t	index;
 
-	printf("%s", tmp);
-	new = malloc (sizeof(char) * ft_strlen(tmp) + 1);
-	if (!new)
-		return (NULL);
-		while (ft_strlen(tmp) - index > ft_strlen(tmp) / 2)
+	while (nb / 10 > 0)
 	{
-		tmp[index] = tmp[ft_strlen(tmp) - index - 1];
-		printf("_______\ntmp : %s\n", tmp);
+		unit = nb % 10;
+		nb = nb / 10;
+		new[index++] = unit + '0';
+	}
+	new[index++] = nb + '0';
+	printf("%zu\n", index);
+	return (index);
+}
+
+void	reverse(char *new)
+{
+	char	tmp;
+	int		len;
+	size_t	index;
+
+	len = ft_strlen(new);
+	index = 0;
+	while (index * 2 < len)
+	{
+		tmp = new[index];
+		new[index] = new[len - 1 - index];
+		printf("%c\n", new[index]);
+		new[len - index] = tmp;
 		index++;
 	}
-	return (new);
 }
 
 char	*ft_itoa(int n)
 {
-	char		tmp[12];
 	char		*new;
 	long long	nb;
-	int			unit;
 	size_t		index;
 
+	new = malloc(sizeof(char) * 12);
 	index = 0;
 	nb = n;
 	if (nb < 0)
 	{
 		nb = nb * -1;
-		tmp[index++] = '-';
+		index = nbr(&new[1], nb);
+		index++;
+		new[0] = '-';
 	}
-	while (nb / 10 > 0)
-	{
-		unit = nb % 10;
-		nb = nb / 10;
-		tmp[index++] = unit + '0';
-	}
-	tmp[index++] = unit * 2 + '0';
-	tmp[index] = '\0';
-	printf("tmp : %s\n", tmp);
-	new = reverse(tmp);
+	else
+		index = nbr(new, nb);
+	index++;
+	new[index] = '\0';
+	printf("tmp : %s\n", new);
+	reverse(&new[1]);
 	return (new);
 }
 
