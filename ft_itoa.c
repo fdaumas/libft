b9c57b20371afa9ccc98bc1e12b3e6c05421f89e
fdaumas <florian.daumas.git@gmail.com>
2021-11-19 02:31:45 +0100

    itoa finish without tester norm ok expet on 0 ok

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: fdaumas <fdaumas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 17:16:05 by fdaumas           #+#    #+#             */
-/*   Updated: 2021/11/18 23:44:11 by fdaumas          ###   ########.fr       */
+/*   Updated: 2021/11/19 02:30:33 by fdaumas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ size_t	nbr(char *new, long long nb)
 		new[index++] = unit + '0';
 	}
 	new[index++] = nb + '0';
-	printf("%zu\n", index);
 	return (index);
 }
 
@@ -36,14 +35,20 @@ void	reverse(char *new)
 
 	len = ft_strlen(new);
 	index = 0;
-	while (index * 2 < len)
+	while (index * 2 < len - 1)
 	{
 		tmp = new[index];
 		new[index] = new[len - 1 - index];
-		printf("%c\n", new[index]);
-		new[len - index] = tmp;
+		new[len - 1 - index] = tmp;
 		index++;
 	}
+}
+
+char	*expet(char *new)
+{
+	new[0] = '0';
+	new[1] = '\0';
+	return (new);
 }
 
 char	*ft_itoa(int n)
@@ -53,21 +58,25 @@ char	*ft_itoa(int n)
 	size_t		index;
 
 	new = malloc(sizeof(char) * 12);
+	if (!new)
+		return (NULL);
 	index = 0;
 	nb = n;
-	if (nb < 0)
+	if (n < 0)
 	{
 		nb = nb * -1;
 		index = nbr(&new[1], nb);
-		index++;
 		new[0] = '-';
 	}
+	else if (nb == 0)
+		return (expet(new));
 	else
 		index = nbr(new, nb);
-	index++;
 	new[index] = '\0';
-	printf("tmp : %s\n", new);
-	reverse(&new[1]);
+	if (n < 0)
+		reverse(&new[1]);
+	else
+		reverse(new);
 	return (new);
 }
 
@@ -75,8 +84,7 @@ int	main(void)
 {
 	char	*test;
 
-	test = ft_itoa(-2147483648);
+	test = ft_itoa(0);
 	printf("%s\n", test);
 	free(test);
-	printf("hello world\n");
 }
